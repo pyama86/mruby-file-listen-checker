58edf26f73c390f0e1d885bb4e961eeb7c2cd39d
pyama86 <pyama@pepabo.com>
2019-10-16 23:02:33 +0900

    debug

--- test/mrb_filelistencheck.rb
+++ test/mrb_filelistencheck.rb
@@ -6,7 +6,7 @@ assert("FileListenCheck#listen?") do
   v4 = Process.fork do
     system "nc -l -4 -p 11111"
   end
-  sleep 1
+  sleep 2
 
   assert_true(FileListenCheck.new("0.0.0.0", 11111).listen?)
   assert_false(FileListenCheck.new("0.0.0.0", 11112).listen?)
@@ -18,8 +18,11 @@ assert("FileListenCheck#listen6?") do
   v6 = Process.fork do
     system "nc -l -6 -p 11112"
   end
-  sleep 1
+  sleep 2
 
+  puts "="*30
+  puts `netstat -an`
+  puts "="*30
   assert_false(FileListenCheck.new("::", 11111).listen6?)
   assert_true(FileListenCheck.new("::", 11112).listen6?)
   Process.kill :TERM, v6
